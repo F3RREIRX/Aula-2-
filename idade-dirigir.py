2703e7df44d3d945868eddb1e2ab448a5cfbef17
F3RREIRX <lucasgabrieldeabreuferreira2@gmail.com>
2025-05-30 16:09:52 -0300

    idade-dirigir.py

---
+++ idade-dirigir.py
@@ -0,0 +1,8 @@
+print('Vamos ver se você pode dirigir')
+Pergunta= int(input('Você tem quantos anos? '))
+
+if Pergunta>=18:
+   print('Você pode dirigir')
+   print('Pegue as chaves')
+else:
+    print('Você não pode dirigir')
