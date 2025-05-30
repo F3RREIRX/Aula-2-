bb70e4d4a99815834f59b8b96e61dec0f93b012d
F3RREIRX <lucasgabrieldeabreuferreira2@gmail.com>
2025-05-30 16:10:25 -0300

    idade-filme.py

---
+++ idade-filme.py
@@ -0,0 +1,7 @@
+print('Vamos olhar um filme!!!')
+Pergunta= int(input('Você tem quantos anos? '))
+
+if Pergunta>=18:
+   print('Você tem a idade da classe indicativa para ver esse filme')
+else:
+    print('Você não tem a idade da classe indicativa para ver esse filme')
