f86a697d46e45d1d13b6b8ceddb7d04353b87357
F3RREIRX <lucasgabrieldeabreuferreira2@gmail.com>
2025-05-30 16:12:35 -0300

    media.py

---
+++ media.py
@@ -0,0 +1,5 @@
+nome = str(input('digite seu nome: ')) 
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+media = (n1+n2)/ 2
+print ('Seu nome é ',nome, ' sua primeira nota é ' ,n1, ', a segunda é' ,n2, ' e sua media é ' ,media)
